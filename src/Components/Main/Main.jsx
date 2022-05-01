af89b674969ddc5da6285ac613e9a4fc18c7e8f0
Cesar-Sandoval99 <cesar.iber99@gmail.com>
2022-04-30 21:50:16 -0500

    second commit

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -5,7 +5,7 @@ const Main = () =>{
     return(
         <header>
             <h1>Weather</h1>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJzDCikwQqOBp-sFTHOai7fBuZRH9OCqXB-9KUWdBlXw3DgzC4gTvF4geJ3lxSEQ6-pyI&usqp=CAU" alt="weather" />
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJzDCikwQqOBp-sFTHOai7fBuZRH9OCqXB-9KUWdBlXw3DgzC4gTvF4geJ3lxSEQ6-pyI&usqp=CAU" alt="weather-img" />
         </header>
     )
 }
